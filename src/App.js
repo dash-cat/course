e95db58d363f5f6dd189928dad528ea38d279b9a
greggy <sudakovgrigory@gmail.com>
2023-11-27 17:00:11 +0700

    sotring

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import "../src/styles/App.css";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MySelect from "./components/UI/select/MySelect";
 
 const App = () => {
-
   const [posts, setPosts] = useState([
     { id: 1, title: "LuluHana", body: "tut choto drugoe" },
     { id: 2, title: "LuluHana2", body: "tut choto drugoe" },
@@ -14,18 +14,39 @@ const App = () => {
     { id: 6, title: "LuluHana6", body: "tut choto drugoe" },
   ]);
 
+  const [selectedSort, setSelectedSort] = useState('')
+
   const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-  }
+    setPosts([...posts, newPost]);
+  };
 
   const deletePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts.sort((a,b) => a[sort].localeCompare(b[sort]))])
   }
 
   return (
     <div className="App">
-      <PostForm create={createPost}/>
-      <PostList remove={deletePost} posts={posts} title='Post for JS'/>
+      <PostForm create={createPost} />
+      <hr style={{margin: '15px 0px'}}/>
+      <MySelect
+        value={selectedSort}
+        onChange={sortPosts}
+        defaultValue="Sorting by..."
+        options={[
+          {value: 'title', name: 'By name'},
+          {value: 'body', name: 'By description'},
+        ]}
+      />
+      {posts.length ? (
+        <PostList remove={deletePost} posts={posts} title="Post for JS" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Postov NET!</h1>
+      )}
     </div>
   );
 };
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import MyButton from "./UI/button/MyButton";
 
 const Post = (props) => {
-    console.log(props)
     return (
         <div className='post'>
         <div className='post__content'>
