b49d76e145fa149a8e1784f454a7e49354d715f3
greggy <sudakovgrigory@gmail.com>
2023-11-28 13:54:17 +0700

    Link and Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import PostPage from "./pages/PostPage";
 import "../src/styles/App.css";
+import {BrowserRouter, Route, Routes}from 'react-router-dom'
+import About from "./pages/About";
+import Navbar from "./components/UI/navbar/Navbar";
 
 const App = () => {
  
   return (
-    <div className="App">
-      <PostPage/>
-     </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<PostPage/>}/>
+      </Routes>
+     </BrowserRouter>
   );
 };
 export default App;
