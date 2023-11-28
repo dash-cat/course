78cd8b92fc969a842618b2b64663f40d3e378500
greggy <sudakovgrigory@gmail.com>
2023-11-28 17:10:23 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React from "react";
-import PostPage from "./pages/PostPage";
 import "../src/styles/App.css";
-import {BrowserRouter, Route, Routes}from 'react-router-dom'
-import About from "./pages/About";
-import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 const App = () => {
  
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/" element={<PostPage/>}/>
-      </Routes>
-     </BrowserRouter>
+    <AppRouter/>
   );
 };
 export default App;
