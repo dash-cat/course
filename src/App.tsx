99cb378cfe82cf618f021d5fd4851bd1078b6321
greggy <sudakovgrigory@gmail.com>
2023-12-07 13:58:07 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  
+
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>This</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
